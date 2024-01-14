43323f854713c17f6bfc94fa0f5a2887c8fe5f24
1875999831 <64409660+1875999831@users.noreply.github.com>
2024-01-14 17:06:57 +0800

    Update114

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -15,13 +15,17 @@ namespace KID
         public int[] slimesScore = { 10, 20, 30, 40, 50, 60, 70, 80 };
 
         private int totalScore;
-
+        //单例
         public static ScoreManager instance;
+        //分数决定生成
+        public int maxSlimeIndex = 2;
 
         private void Awake()
         {
             instance = this;
         }
+
+    
         /// <summary>
         /// 加分数
         /// </summary>
@@ -32,8 +36,16 @@ namespace KID
             totalScore += Score;
             textScore.text = totalScore.ToString();
 
+            ChangeMaxSlimeIndex();
         }
-        
+        /// <summary>
+        /// 分数决定如何生成
+        /// </summary>
+        private void ChangeMaxSlimeIndex()
+        {
+            if (totalScore >= 500) maxSlimeIndex = 6;
+            else if (totalScore >=100) maxSlimeIndex = 4;
+        }
     }
 
 }
